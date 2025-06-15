c9648b8ff24093d7f68e9fff1063a6193d8e18a3
thiagocalvi <thiagohcalvi@gmail.com>
2025-06-15 16:01:14 -0300

    Adicionando porta do Clock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Configurações
 HOST = '127.0.0.1'  # Endereço localhost
+PORT_CLOCK = 4000 # Porta do Clock
 PORT_EMISSOR = 4001 # Porta do Emissor de Tarefas 
 PORT_ESCALONADOR = 4002 # Porta do Escalonador de Tarefas 
 CLOCK_DELAY_MS = 100 # Delay do clock em milissegundos 

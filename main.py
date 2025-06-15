c00c116e29bf8d240429f234bcfdcc281ccf8e46
thiagocalvi <thiagohcalvi@gmail.com>
2025-06-15 15:56:32 -0300

    Adicionando arquivo principal com as configurações de execução

---
+++ main.py
@@ -0,0 +1,7 @@
+# Configurações
+HOST = '127.0.0.1'  # Endereço localhost
+PORT_EMISSOR = 4001 # Porta do Emissor de Tarefas 
+PORT_ESCALONADOR = 4002 # Porta do Escalonador de Tarefas 
+CLOCK_DELAY_MS = 100 # Delay do clock em milissegundos 
+EMISSOR_ESCALONADOR_DELAY_MS = 5 # Atraso entre o envio para Emissor e Escalonador 
+
